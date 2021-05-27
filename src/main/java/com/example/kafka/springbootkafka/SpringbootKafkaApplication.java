c9713919d6bc2e06c79983aa614877827c1723a7
Fernando <generatof@gmail.com>
2021-05-27 13:41:41 -0300

    Initial commit

---
+++ src/main/java/com/example/kafka/springbootkafka/SpringbootKafkaApplication.java
@@ -0,0 +1,18 @@
+package com.example.kafka.springbootkafka;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootKafkaApplication {
+
+	@Override
+	public String toString() {
+		return super.toString();
+	}
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringbootKafkaApplication.class, args);
+	}
+
+}
